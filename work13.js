f92ea2017311bc4efc26960eafc1029cc7143079
hitoshi-shimura <nagoyamarunouchi07@gmail.com>
2023-04-11 17:53:33 +0900

    slider page

---
+++ work13.js
@@ -0,0 +1,7 @@
+let array = [0, 1, 2, 3, 4];
+let len = array.length;
+function calc_array_length(array_length) {
+    let result = array_length*30;
+    return result;
+}
+document.write(calc_array_length(len)); //引数に変数を格納
